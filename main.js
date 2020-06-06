7ac0b2fba4cc345b58e0fea646a18e8818be1fc5
fantasyxingx <53432580+fantasyxingx@users.noreply.github.com>
2020-06-06 13:48:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+window.onload=function(){
+    //document.write("hello");
+}
+*/
+let dish_picture=[
+    {
+        "type" : "滷肉飯",
+        "url" : "images/滷肉飯.jpg"
+    },
+    {
+        "type" : "拉麵",
+        "url" : "images/拉麵.jpg"
+    },
+    {
+        "type" : "水餃",
+        "url" : "images/水餃.jpg"
+    }
+];
+
+$(document).ready(function(){
+    $("#select_button").click(function(){
+        let number_of_li_length=$("li").length;
+        let random_number= Math.floor(Math.random()*number_of_li_length);
+        $("H1").text($("li").eq(random_number).text());
+        //$("#dish_picture").attr("src",$("li").eq(random_number).text());
+        for(let i=0;i<3;i++)    ';lk '
+            if(dish_picture[i].type==$("li").eq(random_number).text())
+                $("#dish_picture").attr("src",dish_picture[i].url);
+
+    });
+});
